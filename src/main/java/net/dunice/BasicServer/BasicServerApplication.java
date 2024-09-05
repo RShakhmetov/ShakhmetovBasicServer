a87ab4cf96b9923ba0b01be7386b9a4de409724f
rshah812 <r.shakhmetov@stv.dunice.net>
2024-09-05 14:59:20 +0300

    Init commit

---
+++ src/main/java/net/dunice/BasicServer/BasicServerApplication.java
@@ -0,0 +1,14 @@
+package net.dunice.BasicServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicServerApplication.class, args);
+
+	}
+
+}
