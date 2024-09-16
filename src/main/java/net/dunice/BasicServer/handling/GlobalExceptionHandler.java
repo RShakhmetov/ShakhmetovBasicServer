635c9103e29d7b023519340a29e673f99ae0acc4
rshah812 <r.shakhmetov@stv.dunice.net>
2024-09-16 14:41:56 +0300

    Fix mistakes

--- src/main/java/net/dunice/BasicServer/handling/GlobalExceptionHandler.java
+++ src/main/java/net/dunice/BasicServer/handling/GlobalExceptionHandler.java
@@ -15,48 +15,35 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseSuccessResponse> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException ex
-    ) {
+    public ResponseEntity<BaseSuccessResponse>
+        handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         var e = ex.getBindingResult().getAllErrors();
-        Integer err = e.stream()
-                .map(item -> ErrorCodes.ERROR_CODE_MAP.get(item.getDefaultMessage()))
+        Integer err = e
+                .stream()
+                .map(item -> ErrorCodes.ERROR_CODE_MAP
+                .get(item.getDefaultMessage()))
                 .findFirst()
                 .orElse(0);
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new BaseSuccessResponse(err));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseSuccessResponse(err));
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<BaseSuccessResponse> handleConstraintViolationException(
-            ConstraintViolationException ex
-    ) {
+    public ResponseEntity<BaseSuccessResponse> handleConstraintViolationException(ConstraintViolationException ex) {
         var e = ex.getConstraintViolations();
-        List<Integer> err = e.stream().map(item -> ErrorCodes.ERROR_CODE_MAP.get(item.getMessage()))
-                .toList();
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new BaseSuccessResponse(err.getFirst(), err));
+        List<Integer> err = e.stream().map(item -> ErrorCodes.ERROR_CODE_MAP.get(item.getMessage())).toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseSuccessResponse(err.getFirst(), err));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<BaseSuccessResponse> handleHttpMessageNotReadableException(
-            HttpMessageNotReadableException ex
-    ) {
+    public ResponseEntity<BaseSuccessResponse>
+        handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         Integer err = ErrorCodes.ERROR_CODE_MAP.get(ErrorCodes.HTTP_MESSAGE_NOT_READABLE_EXCEPTION.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new BaseSuccessResponse(err));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseSuccessResponse(err));
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<BaseSuccessResponse> handleNoSuchElementException(
-            NoSuchElementException ex
-    ) {
+    public ResponseEntity<BaseSuccessResponse> handleNoSuchElementException(NoSuchElementException ex) {
         Integer err = ErrorCodes.ERROR_CODE_MAP.get(ErrorCodes.TASK_NOT_FOUND.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new BaseSuccessResponse(err));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseSuccessResponse(err));
     }
 }
